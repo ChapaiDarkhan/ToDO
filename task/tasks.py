49363c538f75b210fa2206a7ec027c33f6988a13
darkhan <chapaid8@gmail.com>
2022-07-22 19:23:22 +0600

    [TODO-13] Optimization send_message task

--- task/tasks.py
+++ task/tasks.py
@@ -11,12 +11,11 @@ def send_message(user_id, task_id):
     user = User.objects.get(id=user_id)
     subject = f'You change the status of the task, task id:{task_id}'
     task = Task.objects.filter(id=task_id).first()
-    print(task)
     message = f"You change the task status, task information here:" \
-              f"'id': {task.get('id')}," \
-              f"'title': {task.get('title', '')}," \
-              f"'description': {task.get('description', '')}," \
-              f"'execution_period': {task.get('execution_period', '')}," \
-              f"'executed': {task.get('executed', '')}"
+              f"'id': {task.id}," \
+              f"'title': {task.title}," \
+              f"'description': {task.description}," \
+              f"'execution_period': {task.execution_period}," \
+              f"'is_executed': {task.is_executed}"
     send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
 
